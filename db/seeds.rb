584c1110d33d338509a649de394fabb366d836d4
juan267 <j.gomez267@gmail.com>
2015-02-24 13:03:56 -0500

    added activation link

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-User.create!(name: "Example User", email: 'a123@example.com', password: "foobar", password_confirmation: "foobar", admin: true)
+User.create!(name: "Example User", email: 'a123@example.com', password: "foobar", password_confirmation: "foobar", admin: true, activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
   email = "user-#{n+1}@example.com"
   password = "password"
-  User.create!(name: name, email: email, password: password, password_confirmation: password)
+  User.create!(name: name, email: email, password: password, password_confirmation: password, admin: true, activated: true, activated_at: Time.zone.now)
 end
