a9389658073918915bad2bebfa966be142d936b6
juan267 <j.gomez267@gmail.com>
2015-03-01 21:10:00 -0500

    change send=grid for mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,13 +49,12 @@ Rails.application.configure do
   host = 'juan-rails-sample.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
     :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :address        => 'smtp.mandrillapp.com',
+    :user_name      => ENV['MANDRILL_USERNAME'],
+    :password       => ENV['MANDRILL_APIKEY'],
     :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    :authentication => :plain,
   }
 
   # Use the lowest log level to ensure availability of diagnostic information
